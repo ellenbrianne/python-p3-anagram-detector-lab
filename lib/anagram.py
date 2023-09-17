baeaadbf0a096526f0482cbf25fcc01efc23dbee
Ellen Bennett <bennette831@gmail.com>
2023-09-17 04:10:29 -0600

    create anagram algorithm, use match instance method to compare given word attribute to list of potential anagrams and return matches

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list):
+        match_display = []
+        separated_word = [letter for letter in self.word]
+        for str in list:
+            individual_word = [letter for letter in str]
+            if sorted(separated_word) == sorted(individual_word):
+                match_display.append(str)
+            else:
+                return match_display
